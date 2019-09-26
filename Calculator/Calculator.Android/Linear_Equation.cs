60fb16d8ccee18d2689eb40c0c835e5d74bd69bd
iva_bogdan <iva_bogdan@ukr.net>
2019-09-26 11:27:59 +0300

    Добавьте файлы проекта.

---
+++ Calculator/Calculator.Android/Linear_Equation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Calculator.Droid
+{
+    class Linear_Equation
+    {
+        public string a { get; set; }
+
+        public string x { get; set; }
+
+        public string b { get; set; }
+
+        public Linear_Equation() : this(0,0) { }
+
+        public Linear_Equation(double a, double b)
+        {
+            this.NewEquation(a, b);
+        }
+
+        public string Root() { return x; }
+
+        public string NewEquation(double a, double b)
+        {
+            this.a = Convert.ToString(a);
+            this.b = Convert.ToString(b);
+
+            if (a != 0)
+                this.x = Convert.ToString(-b / a);
+            else if (a == 0)
+                this.x = "Коренів не має!";
+            else if (a == 0 && b == 0)
+                this.x = "Безліч коренів!";
+        }
+    }
+}
---
+++ Calculator/Calculator.Android/Square_Equation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Calculator.Droid
+{
+    class Square_Equation
+    {
+        public string a { get; set; }
+
+        public string b { get; set; }
+
+        public string c { get; set; }
+
+        public string x1 { get; set; }
+
+        public string x2 { get; set; }
+
+        public Square_Equation() : this(0,0,0) { }
+
+        public Square_Equation(double a, double b, double c)
+        {
+            this.a = Convert.ToString(a);
+            this.b = Convert.ToString(b);
+            this.c = Convert.ToString(c);
+
+            double Discriminant = (b * b) - (4 * a * c);
+
+            if (Discriminant < 0)
+            {
+                x1 = "Корені комплексні!";
+                x2 = "Корені комплексні!";
+            }
+            else
+            {
+                x1 = Convert.ToString((-b + Math.Sqrt(Discriminant)) / (2 * a));
+                x2 = Convert.ToString((-b - Math.Sqrt(Discriminant)) / (2 * a));
+            }
+
+        }
+
+        public string Root1() { return x1; }
+
+        public string Root2() { return x2; }
+    }
+}
